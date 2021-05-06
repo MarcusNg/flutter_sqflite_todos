cfa0ee47c83b6c85e1b25f8253c47701f162c4f8
Marcus Ng <MarcusNg@users.noreply.github.com>
2021-05-06 15:16:05 -0400

    add add todo form

--- lib/screens/add_todo_screen.dart
+++ lib/screens/add_todo_screen.dart
@@ -1,4 +1,8 @@
+import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sqflite_todos/models/todo_model.dart';
+import 'package:flutter_sqflite_todos/extensions/string_extension.dart';
+import 'package:intl/intl.dart';
 
 class AddTodoScreen extends StatefulWidget {
   @override
@@ -6,10 +10,112 @@ class AddTodoScreen extends StatefulWidget {
 }
 
 class _AddTodoScreenState extends State<AddTodoScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  late TextEditingController _nameController;
+  late TextEditingController _dateController;
+
+  Todo? _todo;
+
+  @override
+  void initState() {
+    super.initState();
+    _todo = Todo(
+      name: '',
+      date: DateTime.now(),
+      priorityLevel: PriorityLevel.medium,
+      completed: false,
+    );
+
+    _nameController = TextEditingController(text: _todo!.name);
+    _dateController =
+        TextEditingController(text: DateFormat.MMMMEEEEd().format(_todo!.date));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Add Todo'),
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20.0,
+            vertical: 40.0,
+          ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  style: const TextStyle(fontSize: 16.0),
+                  decoration: const InputDecoration(labelText: 'Name'),
+                  validator: (value) =>
+                      value!.trim().isEmpty ? 'Please enter a name' : null,
+                  onSaved: (value) => _todo = _todo!.copyWith(name: value),
+                ),
+                const SizedBox(height: 32.0),
+                TextFormField(
+                  controller: _dateController,
+                  readOnly: true,
+                  style: const TextStyle(fontSize: 16.0),
+                  decoration: const InputDecoration(labelText: 'Date'),
+                  onTap: _handleDatePicker,
+                ),
+                const SizedBox(height: 32.0),
+                DropdownButtonFormField<PriorityLevel>(
+                  value: _todo!.priorityLevel,
+                  icon: const Icon(Icons.arrow_drop_down_circle),
+                  iconSize: 22.0,
+                  iconEnabledColor: Theme.of(context).primaryColor,
+                  items: PriorityLevel.values
+                      .map((priorityLevel) => DropdownMenuItem(
+                            value: priorityLevel,
+                            child: Text(
+                              EnumToString.convertToString(priorityLevel)
+                                  .capitalize(),
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ))
+                      .toList(),
+                  style: const TextStyle(fontSize: 16.0),
+                  decoration:
+                      const InputDecoration(labelText: 'Priority Level'),
+                  onChanged: (value) => setState(
+                    () => _todo = _todo!.copyWith(priorityLevel: value),
+                  ),
+                ),
+                const SizedBox(height: 32.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    minimumSize: const Size.fromHeight(45.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Add',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
+
+  void _handleDatePicker() {}
 }
